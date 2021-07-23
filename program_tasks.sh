5f83ff068f410c6642f9cfa21f1dfa4041a239ac
amillalen <alvaro.millalen@khipu.com>
2021-07-23 10:02:01 -0400

    test

--- program_tasks.sh
+++ program_tasks.sh
@@ -4,7 +4,7 @@ cd $WORKDIR
 function queue_task_run_once(){
   jobcount=$(atq -q i|wc -l)
   if [ "$jobcount" == "0" ]; then
-    at -q i -f $I3GWORKDIR/once.sh -t 202107230946
+    at -q i -f $I3GWORKDIR/once.sh -t 202107231005
   fi
 }
 
@@ -12,5 +12,5 @@ function clear_task_queue(){
    atq -q i|while read line;do jobn=$(echo "$line"|cut -d' ' -f1); echo "remove job $jobn"; atrm $jobn;done
 }
 
-#queue_task_run_once()
+queue_task_run_once()
 
