a0e753243d2e5ebe0de217aa712540ba176e03da
amillalen <alvaro.millalen@khipu.com>
2021-07-23 18:53:57 -0400

    test

--- program_tasks.sh
+++ program_tasks.sh
@@ -7,7 +7,10 @@ function queue_task_run_once(){
   jobexecdate=$2
   jobcount=$(atq -q i|wc -l)
   now=$(date '+%Y%m%d%H%M')
+  echo "dates $now  $jobexecdate"
+  echo "jobcount $jobcount"
   if [[ "$jobcount" == "0"  && "$now" -le  "$jobexecdate" ]]; then
+    echo "progamming job"
     at -q i -f $jobscript -t $jobexecdate
   fi
 }
@@ -16,4 +19,4 @@ function clear_task_queue(){
    atq -q i|while read line;do jobn=$(echo "$line"|cut -d' ' -f1); echo "remove job $jobn"; atrm $jobn;done
 }
 
-queue_task_run_once "$I3GTASKDIR/once.sh" 202107231848
+queue_task_run_once "$I3GTASKDIR/once.sh" 202107231900
