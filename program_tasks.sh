76828e2f86c9ac898cb12a64cb9dcfb9e427b4fa
amillalen <alvaro.millalen@khipu.com>
2021-07-23 18:43:36 -0400

    test

--- program_tasks.sh
+++ program_tasks.sh
@@ -16,4 +16,4 @@ function clear_task_queue(){
    atq -q i|while read line;do jobn=$(echo "$line"|cut -d' ' -f1); echo "remove job $jobn"; atrm $jobn;done
 }
 
-queue_task_run_once "$I3GTASKDIR/once.sh" 202107231810
+queue_task_run_once "$I3GTASKDIR/once.sh" 202107231848
